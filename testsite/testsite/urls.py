8a4d6ec7e18c306e5ed3e1d48a025093551a250b
imSaDNightMare <hamedgh138171@gmail.com>
2024-05-12 16:59:12 +0330

    url patterns
    i importes css/img/js in my project

--- testsite/testsite/urls.py
+++ testsite/testsite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for testsite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,15 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path
 
 urlpatterns = [
-    path('',include('website.urls')),
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
-
 ]
-urlpatterns += static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
